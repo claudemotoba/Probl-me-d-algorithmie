bddf9e815810d9ff574f1add620bd7edab8bd209
Your Name <you@example.com>
2020-03-09 08:00:57 +0100

    modif

--- script.js
+++ script.js
@@ -4,99 +4,100 @@ let maphrase = "Bonjour tout le monde,je suis Christian Lisangola,développeur l
 
 let maNouvellePhrase = maphrase.toLowerCase().split(/[ ,]/);
 
-console.log(maNouvellePhrase);
-console.log(maNouvellePhrase.length);
+// console.log(maNouvellePhrase);
+// console.log(maNouvellePhrase.length);
 
 
 // nombre d'adresse mail
 
-const listeDesAdressesMails = maNouvellePhrase.filter ((adresseMail) => {
+// const listeDesAdressesMails = maNouvellePhrase.filter ((adresseMail) => {
 
-    return adresseMail.includes('@')  
-})
+//     return adresseMail.includes('@')  
+// })
 
-console.log(listeDesAdressesMails);
-console.log(listeDesAdressesMails.length);
+// console.log(listeDesAdressesMails);
+// console.log(listeDesAdressesMails.length);
 
 
 //numero de telephone
 
-const listeDesNumeroDeTelephone = maNouvellePhrase.filter ((adresseMail) => {
-    return adresseMail.includes('+243')
-})
+// const listeDesNumeroDeTelephone = maNouvellePhrase.filter ((adresseMail) => {
+//     return adresseMail.includes('+243')
+// })
 
 const supprimerLePoint = (monTableauDeNumeo)=>{
-    for(let i = 0; i < tableauNumero.length; i++){
+    for(let i = 0; i < monTableauDeNumeo.length; i++){
         if(monTableauDeNumeo[i].endsWith('.')){
             monTableauDeNumeo[i] = monTableauDeNumeo[i].replace('.', '');      
         }
     }
 };
-supprimerLePoint(listeDesNumeroDeTelephone);
-console.log(listeDesNumeroDeTelephone);
-console.log(listeDesNumeroDeTelephone.length);
+// supprimerLePoint(listeDesNumeroDeTelephone);
+// console.log(listeDesNumeroDeTelephone);
+// console.log(listeDesNumeroDeTelephone.length);
 
 // numero de telephone vodacom
 
-const numeroDeTelephoneVodacom = maNouvellePhrase.filter (function(word){ 
-    return word.startsWith('+24381') || word.startsWith('+24382');
-});
-supprimerLePoint(numeroDeTelephoneVodacom);
-console.log(numeroDeTelephoneVodacom);
-console.log(numeroDeTelephoneVodacom.length);
+// const numeroDeTelephoneVodacom = maNouvellePhrase.filter (function(word){ 
+//     return word.startsWith('+24381') || word.startsWith('+24382');
+// });
+// supprimerLePoint(numeroDeTelephoneVodacom);
+// console.log(numeroDeTelephoneVodacom);
+// console.log(numeroDeTelephoneVodacom.length);
 
 
 // numero tigo
 
-const numeroDeTelephoneTigo = maNouvellePhrase.filter (function(word){ 
-    return word.startsWith('+24389') || word.startsWith('+24380');
-});
-supprimerLePoint(numeroDeTelephoneTigo);
-console.log(numeroDeTelephoneTigo);
-console.log(numeroDeTelephoneTigo.length);
+// const numeroDeTelephoneTigo = maNouvellePhrase.filter (function(word){ 
+//     return word.startsWith('+24389') || word.startsWith('+24380');
+// });
+// supprimerLePoint(numeroDeTelephoneTigo);
+// console.log(numeroDeTelephoneTigo);
+// console.log(numeroDeTelephoneTigo.length);
 
 
 // numero orange
 
-const numeroDeTelephoneOrange = maNouvellePhrase.filter (function(word){ 
-    return word.startsWith('+24384') || word.startsWith('+24385');
-});
-supprimerLePoint(numeroDeTelephoneOrange);
-console.log(numeroDeTelephoneOrange);
-console.log(numeroDeTelephoneOrange.length);
+// const numeroDeTelephoneOrange = maNouvellePhrase.filter (function(word){ 
+//     return word.startsWith('+24384') || word.startsWith('+24385');
+// });
+// supprimerLePoint(numeroDeTelephoneOrange);
+// console.log(numeroDeTelephoneOrange);
+// console.log(numeroDeTelephoneOrange.length);
 
 
 // numero airtel
 
-const numeroDeTelephoneOrange = maNouvellePhrase.filter (function(word){ 
-    return word.startsWith('+24397') || word.startsWith('+24399');
-});
-supprimerLePoint(numeroDeTelephoneOrange);
-console.log(numeroDeTelephoneOrange);
-console.log(numeroDeTelephoneOrange.length);
+// const numeroDeTelephoneOrange = maNouvellePhrase.filter (function(word){ 
+//     return word.startsWith('+24397') || word.startsWith('+24399');
+// });
+// supprimerLePoint(numeroDeTelephoneOrange);
+// console.log(numeroDeTelephoneOrange);
+// console.log(numeroDeTelephoneOrange.length);
 
 
-// adresse de messagerie professionlle
+// // adresse de messagerie professionlle
 
-const messagerieProfessionlle = maNouvellePhrase.filter (function(word){ 
-    return word.includes('@kinshasadigital')
-});
-console.log(messagerieProfessionlle);
-console.log(messagerieProfessionlle.length);
+// const messagerieProfessionlle = maNouvellePhrase.filter (function(word){ 
+//     return word.includes('@kinshasadigital')
+// });
+// console.log(messagerieProfessionlle);
+// console.log(messagerieProfessionlle.length);
 
 
-// messagerie privee
+// // messagerie privee
 
-const messageriePrivee = maNouvellePhrase.filter (function(word){ 
-    return word.includes('@gmail')
-});
-console.log(messageriePrivee);
-console.log(messageriePrivee);
+// const messageriePrivee = maNouvellePhrase.filter (function(word){ 
+//     return word.includes('@gmail')
+// });
+// supprimerLePoint(messageriePrivee);
+// console.log(messageriePrivee);
+// console.log(messageriePrivee.length);
 
 
 // supprimer les mots
 
-const motAsupprimer = maNouvellePhrase.filter(word=> !/le|à|la|je|une|un|de|qui|est|mon|0|1|et|par|nous|les|@/.test(word))
-console.log(motAsupprimer );
-let nombreDeMot = motAsupprimer.length
-console.log(nombreDeMot);
+// const motAsupprimer = maNouvellePhrase.filter(word=> !/le|à|la|je|une|un|de|qui|est|mon|0|1|et|par|nous|les|@/.test(word))
+// console.log(motAsupprimer );
+// let nombreDeMot = motAsupprimer.length
+// console.log(nombreDeMot);
